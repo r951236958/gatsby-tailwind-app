874ea75aa6338be8dce88f723c9a1b6bb620d3d3
nick <r951236958@gmail.com>
2021-03-19 18:22:21 +0800

    更新ThemeToggle

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -1,49 +1,50 @@
-import React, { useEffect, useState } from "react"
-import MoonStarIcon from "./MoonStarIcon"
-import SunIcon from "./SunIcon"
-import "./ThemeToggle.css"
+import React, { useEffect, useState } from "react";
+import MoonStarIcon from "./MoonStarIcon";
+import SunIcon from "./SunIcon";
+import "./ThemeToggle.css";
 
 const updateTheme = isDarkEnabled => {
   // Get CSS variables for background/foreground
-  const styles = getComputedStyle(document.body)
-  const black = styles.getPropertyValue("--black")
-  const white = styles.getPropertyValue("--white")
-  const docEl = document.documentElement
+  const styles = getComputedStyle(document.body);
+  const black = styles.getPropertyValue("--black");
+  const white = styles.getPropertyValue("--white");
+  const docEl = document.documentElement;
 
   if (isDarkEnabled) {
-    docEl.style.setProperty("--background", black)
-    docEl.style.setProperty("--foreground", white)
-    document.querySelector("html").classList.add("darkmode")
+    docEl.style.setProperty("--background", black);
+    docEl.style.setProperty("--foreground", white);
+    document.querySelector("html").classList.add("darkmode");
   } else {
-    docEl.style.setProperty("--background", white)
-    docEl.style.setProperty("--foreground", black)
-    document.querySelector("html").classList.remove("darkmode")
+    docEl.style.setProperty("--background", white);
+    docEl.style.setProperty("--foreground", black);
+    document.querySelector("html").classList.remove("darkmode");
   }
-}
+};
 
 const ThemeToggle = () => {
-  const [isEnabled, setIsEnabled] = useState(false)
+  const [isEnabled, setIsEnabled] = useState(false);
 
   useEffect(() => {
-    updateTheme(isEnabled)
-  }, [isEnabled])
+    updateTheme(isEnabled);
+  }, [isEnabled]);
 
   const toggleState = () => {
-    setIsEnabled(prevState => !prevState)
-  }
+    setIsEnabled(prevState => !prevState);
+  };
 
   return (
     <>
-      <label htmlFor="toggle" className="toggle-wrapper">
+      <label htmlFor="toggle" className="box-border toggle-wrapper">
         <div className={`toggle ${isEnabled ? "enabled" : "disabled"}`}>
-          <span className="hidden-toggle">
+          <span className="hidden">
             {isEnabled ? "Enable Light Mode" : "Enable Dark Mode"}
           </span>
-          <div className="icons">
+          <div className="space-x-1 icons">
             <SunIcon />
             <MoonStarIcon />
           </div>
           <input
+            className="hidden"
             id="toggle"
             name="toggle"
             type="checkbox"
@@ -53,7 +54,7 @@ const ThemeToggle = () => {
         </div>
       </label>
     </>
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types"
 import React, { useEffect, useState } from "react"
 import { menuList } from "../../config"
 import { getUser, isLoggedIn, logout } from "../../utils/auth"
-import ThemeSwitch from '../ThemeSwitch'
+import ThemeToggle from "../ThemeToggle"
 
 export default function TopBar({ siteTitle }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -31,7 +31,7 @@ export default function TopBar({ siteTitle }) {
         <div className="px-2 mx-auto max-w-7xl md:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-              {/* Mobile menu button*/}
+              {/* Mobile menu button */}
               <button
                 className="inline-flex items-center justify-center p-2 text-gray-300 rounded-md hover:text-white hover:bg-gray-700 hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-expanded="false"
@@ -48,7 +48,7 @@ export default function TopBar({ siteTitle }) {
                 <svg
                   width="27"
                   height="27"
-                  className={(!menuOpen ? 'block' : 'hidden') + ' h-6 w-6'}
+                  className={(!menuOpen ? "block" : "hidden") + " h-6 w-6"}
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -71,7 +71,7 @@ export default function TopBar({ siteTitle }) {
                   width="27"
                   height="27"
                   className={
-                    (menuOpen ? 'block md:hidden' : 'hidden') + ' h-6 w-6'
+                    (menuOpen ? "block md:hidden" : "hidden") + " h-6 w-6"
                   }
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -160,14 +160,14 @@ export default function TopBar({ siteTitle }) {
                   </button>
                 </div>
               </div>
-              <ThemeSwitch />
+              <ThemeToggle />
             </div>
           </div>
         </div>
 
         {/* Mobile menu, show/hide based on menu state. */}
         <div
-          className={menuOpen ? 'block md:hidden' : 'hidden'}
+          className={menuOpen ? "block md:hidden" : "hidden"}
           id="mobile-menu"
         >
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
@@ -206,7 +206,7 @@ export default function TopBar({ siteTitle }) {
 
                 <button
                   className="flex-shrink-0 p-1 ml-auto text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  onClick={async (event) => {
+                  onClick={async event => {
                     event.preventDefault()
                     await logout(firebase).then(() => navigate(`/app/login`))
                   }}
@@ -237,7 +237,7 @@ export default function TopBar({ siteTitle }) {
 
                 <button
                   className="block px-3 py-2 text-base font-medium text-gray-400 rounded-md hover:no-underline hover:text-white hover:bg-gray-700"
-                  onClick={async (event) => {
+                  onClick={async event => {
                     event.preventDefault()
                     await logout(firebase).then(() => navigate(`/app/login`))
                   }}
